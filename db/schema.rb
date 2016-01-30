1c6a46f75df1ded35f44b76ba9c3a2b5f02d2128
AKSHATA <akshata.s.shetty@ril.com>
2016-01-30 16:10:48 +0530

    Added users migration and added user_id to articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130075206) do
+ActiveRecord::Schema.define(version: 20160130103446) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "mobile"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
